200d4406cf9c2b421aa86b88b9a1c914eb144c00
hwangjeyeon <kingpele9018@gmail.com>
2023-12-29 05:23:36 +0900

    Feat: 회원가입, 로그인 DTO 추가

---
+++ src/main/java/gdscnamuton/Independent/dto/LoginDto.java
@@ -0,0 +1,10 @@
+package gdscnamuton.Independent.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    private String memberId;
+    private String password;
+}
---
+++ src/main/java/gdscnamuton/Independent/dto/RegisterDto.java
@@ -0,0 +1,14 @@
+package gdscnamuton.Independent.dto;
+
+
+import lombok.Data;
+
+@Data
+public class RegisterDto {
+
+    private String memberId;
+    private String password;
+    private String name;
+
+
+}
