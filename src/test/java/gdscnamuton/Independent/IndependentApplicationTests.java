78fd4eb931eb882ce24d737f33339057defd4f69
hwangjeyeon <kingpele9018@gmail.com>
2023-12-29 05:05:44 +0900

    feat: 최초생성

---
+++ src/test/java/gdscnamuton/Independent/IndependentApplicationTests.java
@@ -0,0 +1,13 @@
+package gdscnamuton.Independent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndependentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
